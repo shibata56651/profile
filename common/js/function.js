6b537ea6a4bb712d8cd5da22ee21b3e0bef46f3b
shibata-taiga <shibata-taiga@mitsue.co.jp>
2020-11-01 15:10:40 +0900

    aaa

---
+++ common/js/function.js
@@ -0,0 +1,16 @@
+﻿
+((win, doc) => {
+  'use strict';
+
+  // ローディングアニメーション
+  win.onload = function() {
+    const spinner = doc.getElementById('loading');
+    spinner.classList.add('loaded');
+  }
+
+// ハンバーガーメニュー
+  const hamburgerHook =  doc.querySelector('.js-hamburger-hook');
+  console.log(document.body)
+  
+  hamburgerHook.classList.add('is-active')
+})(window, document);
